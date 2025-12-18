9cf58d24142379bde6a27828f91dbb92a56b6f7a
temiiiyo <artfrolov28042004@gmail.com>
2025-12-18 18:19:45 +0300

    Delete хз - Топчубаева directory

--- хз - Топчубаева/services.py
+++
@@ -1,9 +0,0 @@
-class Service:
-    def __init__(self, id_service, name, tarif):
-        self.id_service = id_service
-        self.name = name
-        self.tarif = tarif
-
-    def __str__(self):
-        return (f"Услуга: {self.name}, (ID {self.id_service}), "
-                f"Тариф: {self.tarif}")
--- хз - Топчубаева/streets.py
+++
@@ -1,7 +0,0 @@
-class Street:
-    def __init__(self, id_street, name):
-        self.id_street = id_street
-        self.name = name
-
-    def __str__(self):
-        return f"Улица {self.name}, (ID: {self.id_street})"
